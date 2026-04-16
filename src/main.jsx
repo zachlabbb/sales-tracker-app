5cd3faddb5857274f05f87c0ad82b1e5c0c4d009
zachlabbb <zacharylabelle8@gmail.com>
2026-04-15 23:29:21 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SalesTracker from './SalesTracker.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SalesTracker />
+  </React.StrictMode>
+)
